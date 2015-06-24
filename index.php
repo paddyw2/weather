8f0f61696358513705f60899b4449db056c2d630
Patrick <p.withams@gmail.com>
2015-06-24 12:16:02 -0600

    fixing background image issue

--- index.php
+++ index.php
@@ -1,5 +1,3 @@
-
-
 <?php
 include 'weather.php';
 ?>
@@ -8,96 +6,65 @@ include 'weather.php';
 include 'icon_chooser.php';
 ?>
 
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
 <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Weather App</title>
 <meta name="description" content="">
 <link href="css/weatherstyle.css" rel="stylesheet">
-      <link href="dist/css/bootstrap.min.css" rel="stylesheet">
+<link href="dist/css/bootstrap.min.css" rel="stylesheet">
 
-    <!-- Custom styles for this template -->
-    <link href="css/cover.css" rel="stylesheet">
-    <link href="css/weatherstyle.css" rel="stylesheet">
-    <link href="css/icons/flaticon.css" rel="stylesheet">
-     <link href="mobile-menu/rmm-css/responsivemobilemenu.css" rel="stylesheet">
-<!-- IE6-8 support of HTML5 elements --> <!--[if lt IE 9]>
-<script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
-<![endif]-->
+<!-- Custom styles for this template -->
+<link href="css/cover.css" rel="stylesheet">
+<link href="css/weatherstyle.css" rel="stylesheet">
+<link href="css/icons/flaticon.css" rel="stylesheet">
+<link href="mobile-menu/rmm-css/responsivemobilemenu.css" rel="stylesheet">
 </head>
 <body>
-    
     <div class="site-wrapper">
-
-      <div class="site-wrapper-inner">
-
-        <div class="cover-container">
-            
-  
-            <!-- optional menu -->
+        <div class="site-wrapper-inner">
+            <div class="cover-container">
+             <!-- optional menu -->
             <!--
             <div class="rmm">
             <ul>
-                <li><a href='#home'>Home</a></li>
-                <li><a href='#about-me'>About me</a></li>
-                <li><a href='#gallery'>Gallery</a></li>
-                <li><a href='#blog'>Blog</a></li>
-                <li><a href='#links'>Links</a></li>
-                <li><a href='#sitemap'>Sitemap</a></li> 
+            <li><a href='#home'>Home</a></li>
+            <li><a href='#about-me'>About me</a></li>
+            <li><a href='#gallery'>Gallery</a></li>
+            <li><a href='#blog'>Blog</a></li>
+            <li><a href='#links'>Links</a></li>
+            <li><a href='#sitemap'>Sitemap</a></li> 
             </ul>
-        </div>
+            </div>
             -->
-            
-            
-            
-
-          <div class="inner cover">
-              <div class="profileimage <?php echo $weather_icon ?>"></div>
-              
-            <h1 class="cover-heading"><?php echo $location_full ?></h1>
-              <div class="lead weatherdescription"><h2><?php echo $location_weather ?></h2></div>
-              <div class="lead weatherlist">
-            <ul>
-              <li><?php echo $location_temperature."C" ?>  </li>
-                <li><?php echo $location_humidity ?></li>
-                   <li><?php echo $location_windmph."mph" ?></li>
-               
-              
-                  </ul></div>
-            <p class="lead">
-              <a href="http://www.wunderground.com/" class="btn btn-lg btn-default">Created Using the Wunderground API</a>
-            </p>
-          </div>
-
-          <div class="mastfoot">
-            
-          </div>
-
+                <div class="inner cover">
+                    <div class="profileimage <?php echo $weather_icon ?>"></div>
+                    <h1 class="cover-heading"><?php echo $location_full ?></h1>
+                    <div class="lead weatherdescription"><h2><?php echo $location_weather ?></h2></div>
+                    <div class="lead weatherlist">
+                        <ul>
+                        <li><?php echo $location_temperature."C" ?>  </li>
+                        <li><?php echo $location_humidity ?></li>
+                        <li><?php echo $location_windmph."mph" ?></li>
+                         </ul>
+                    </div>
+                    <p class="lead">
+                        <a href="http://www.wunderground.com/" class="btn btn-lg btn-default">Created Using the Wunderground API</a>
+                    </p>
+                </div>
+            </div>
+        <div class="credit">Icons by <a href="http://www.flaticon.com" title="Flaticon">Flaticon</a>&nbsp;|&nbsp;Image by <a href="https://unsplash.com/gregshield" title="Flaticon">Unsplash</a></div>
         </div>
-<div class="credit">Icons by <a href="http://www.flaticon.com" title="Flaticon">Flaticon</a>&nbsp;|&nbsp;Image by <a href="https://unsplash.com/gregshield" title="Flaticon">Unsplash</a></div>
-      </div>
-
     </div>
-    
-    
 
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-    <script src="/dist/js/bootstrap.min.js"></script>
-    
-  
-    
-    <script src="/mobile-menu/rmm-js/responsivemobilemenu.js"></script>
-  
-    
-    
-    
+
+<!-- JS -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="/dist/js/bootstrap.min.js"></script>
+<script src="/mobile-menu/rmm-js/responsivemobilemenu.js"></script>
+
 </body>
 </html>
     
